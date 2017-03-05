e719d964ef865b443b520b64551b989c6f9fe732
Tyler <tywilly300@gmail.com>
2017-03-05 18:30:06 -0500

    Added library GLEW.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main() {
+
+    float rotation = 0;
 
     if(!glfwInit()){
         std::cout << "Failed to Init GLFW" << std::endl;
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(500,500, "Hello World", NULL, NULL);
+    glewInit();
+
+    GLFWwindow* window = glfwCreateWindow(500,500, "BrownWillyEngine", NULL, NULL);
 
     glfwMakeContextCurrent(window);
+
+    glClearColor(0.0,0.0,0.0,0.0);
 
     while(!glfwWindowShouldClose(window)){
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES);
-
         glColor3f(1.0, 0.0, 0.0);
         glVertex2f(0.0, 1.0);
         glColor3f(0.0, 1.0, 0.0);
@@ -26,10 +32,17 @@ int main() {
         glVertex2f(1.0, -1.0);
         glEnd();
 
+        if(rotation >= 360){
+            rotation = 0;
+        }
+
+        rotation+= 0.001;
+
+        glRotatef(rotation, 0.0,0.0,1.0);
+
         glfwSwapBuffers(window);
 
         glfwPollEvents();
-
     }
 
     glfwTerminate();
